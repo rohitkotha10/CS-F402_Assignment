2514bdd7497c5ae17d8a16366c89aaed82d4a6af
rohitkotha10 <rohitkotha10@gmail.com>
2023-04-11 02:07:12 +0530

    robust collinearity

--- src/line.cpp
+++ src/line.cpp
@@ -1,24 +1,32 @@
-#include <CGAL/Simple_cartesian.h>
 #include <iostream>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
-typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef Kernel::Point_2 Point_2;
-typedef Kernel::Segment_2 Segment_2;
+
+Point_2 assignPoint(std::string p);
+
 int main() {
-    Point_2 p(1.2, 1), q(10, 10.3);
-    std::cout << "p = " << p << std::endl;
-    std::cout << "q = " << q.x() << " " << q.y() << std::endl;
-    std::cout << "sqdist(p,q) = " << CGAL::squared_distance(p, q) << std::endl;
-    Segment_2 s(p, q);
-    Point_2 m(5, 9);
-    std::cout << "m = " << m << std::endl;
-    std::cout << "sqdist(Segment_2(p,q), m) = " << CGAL::squared_distance(s, m) << std::endl;
-    std::cout << "p, q, and m ";
-    switch (CGAL::orientation(p, q, m)) {
-        case CGAL::COLLINEAR: std::cout << "are collinear\n"; break;
-        case CGAL::LEFT_TURN: std::cout << "make a left turn\n"; break;
-        case CGAL::RIGHT_TURN: std::cout << "make a right turn\n"; break;
+    {
+        std::cout
+            << (CGAL::collinear(assignPoint("0, 0.3"), assignPoint("1, 0.6"), assignPoint("2, 0.9"))
+                    ? "collinear\n"
+                    : "not collinear\n");
     }
-    std::cout << " midpoint(p,q) = " << CGAL::midpoint(p, q) << std::endl;
+    {
+        Point_2 p = assignPoint("0, 1.0 / 3.0"), q = assignPoint("1, 2.0 / 3.0"), r = assignPoint("2, 1");
+        std::cout << (CGAL::collinear(p, q, r) ? "collinear\n" : "not collinear\n");
+    }
+    {
+        Point_2 p(0, 0), q(1, 1), r(2, 2);
+        std::cout << (CGAL::collinear(p, q, r) ? "collinear\n" : "not collinear\n");
+    }
     return 0;
+}
+
+Point_2 assignPoint(std::string p) {
+    Point_2 ans;
+    std::istringstream input(p);
+    input >> p;
+    return ans;
 }
